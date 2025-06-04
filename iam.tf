dc36681df778fcc1a16762e3d9ee018a01339dd4
Filipe Gomes <filipeandradegomes@gmail.com>
2025-06-04 08:49:35 -0300

    feat: adding iam configuration to the eks cluster

---
+++ iam.tf
@@ -0,0 +1,23 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(local.tags, { Name = "eks-role" })
+}
+
+resource "aws_iam_role_policy_attachment" "attachment_eks_role_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
---
+++ locals.tf
@@ -0,0 +1,6 @@
+locals {
+  tags = {
+    Department   = "DevOps"
+    Organization = "infrastructure"
+  }
+}
