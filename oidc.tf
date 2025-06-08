c517d9a6d70673d91e2a2843cae937576a0abb68
Filipe Gomes <filipeandradegomes@gmail.com>
2025-06-08 15:02:40 -0300

    fix: terraform fmt

--- oidc.tf
+++ oidc.tf
@@ -2,8 +2,9 @@ data "tls_certificate" "eks_oidc" {
   url = aws_eks_cluster.this.identity[0].oidc[0].issuer
 }
 
-resource "aws_iam_openid_connect_provider" "example" {
+resource "aws_iam_openid_connect_provider" "oidc_provider" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.eks_oidc.certificates[0].sha1_fingerprint]
   url             = aws_eks_cluster.this.identity[0].oidc[0].issuer
+  tags            = merge(local.tags, { Name = "eks-oidc-provider" })
 }
